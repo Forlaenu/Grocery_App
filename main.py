c3855ade3d5c3bc37244c20a84c910058359477a
Forlaenu <93723560+Forlaenu@users.noreply.github.com>
2021-11-16 20:56:37 -0600

    added main. Just basic setup text

---
+++ main.py
@@ -0,0 +1,27 @@
+def main():
+    print()
+
+# Here are the features you need to implement:
+
+# A user should be able to create shopping lists.
+
+# A shopping list will have a name
+
+# A user should be able to select a shopping list and then
+
+# add items
+
+# delete items
+
+# view all items
+# A user should be able to delete a shopping list
+
+# A user should be able to view all shopping lists and their items:
+# Fiesta
+# Milk, Soda, Fish
+
+# Walmart
+# Paper, Napkins, Plate, Chips
+
+# Sams Club
+# Chicken, Beef, Eggs, Sugar, Salt, Pepper, Honey
